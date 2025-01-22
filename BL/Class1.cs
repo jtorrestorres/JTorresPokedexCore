288fe38f1c18eb603e4c456e03dd3aaf878b4be6
jtorrestorres <jtorrstorrs@gmail.com>
2025-01-21 19:41:05 -0600

    Prueba de commit 1

--- BL/Class1.cs
+++ BL/Class1.cs
@@ -3,7 +3,7 @@
     public class Pokemon
     {
         public static ML.Result GetAll()
-        {
+        { //prueba git
             ML.Result result = new ML.Result();
 
             using (DL.JtorresPokedexCoreContext context = new DL.JtorresPokedexCoreContext())
@@ -19,6 +19,40 @@
                     {
 
                      
+
+                    }
+
+                    result.Correct = true;
+                }
+                else
+                {
+                    result.Correct = false;
+                    result.ErrorMessage = "No se encontraron registros";
+                }
+            }
+
+            return result;
+
+        }
+
+
+        public static ML.Result GetById()
+        { //prueba git
+            ML.Result result = new ML.Result();
+
+            using (DL.JtorresPokedexCoreContext context = new DL.JtorresPokedexCoreContext())
+            {
+                var listaMateriasLinq = (from pokemonDB in context.Pokemons
+                                         select pokemonDB).ToList();
+
+                if (listaMateriasLinq.Count > 0)
+                {
+                    result.Objects = new List<object>();
+
+                    foreach (var materia in listaMateriasLinq)
+                    {
+
+
 
                     }
 
